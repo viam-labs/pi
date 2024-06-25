5f32bfe7df52c95813d044927baff1a5aac069e6
Susmita Sanyal <susmita@viam.com>
2024-06-25 12:24:04 -0400

    updates

--- pi.go
+++ pi.go
@@ -7,7 +7,9 @@ import (
 	picommon "go.viam.com/rdk/components/board/pi/common"
 	piImpl "go.viam.com/rdk/components/board/pi/impl"
 	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
+	"go.viam.com/utils"
 )
 
 func init() {
@@ -19,4 +21,26 @@ func init() {
 
 func newPigpio(ctx context.Context, deps resource.Dependencies, conf resource.Config, logger logging.Logger) (board.Board, error) {
 	return piImpl.NewPigpio(ctx, conf.ResourceName(), conf, logger)
+}
+
+func main() {
+	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("pi"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) error {
+	pigpio, err := module.NewModuleFromArgs(ctx, logger)
+
+	if err != nil {
+		return err
+	}
+	pigpio.AddModelFromRegistry(ctx, board.API, picommon.Model)
+
+	err = pigpio.Start(ctx)
+	defer pigpio.Close(ctx)
+	if err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+	return nil
 }
