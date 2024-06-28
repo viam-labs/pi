7962c6b1c21c182a35df58679bffc2cbe452ebb8
Susmita Sanyal <susmita@viam.com>
2024-06-28 15:24:57 -0400

    removing debug function

--- pi/custome-pi.go
+++ pi/custome-pi.go
@@ -2,7 +2,6 @@ package pi
 
 import (
 	"context"
-	"syscall"
 
 	"go.viam.com/rdk/components/board"
 	piImpl "go.viam.com/rdk/components/board/pi/impl"
@@ -21,26 +20,4 @@ func init() {
 
 func newPigpio(ctx context.Context, deps resource.Dependencies, conf resource.Config, logger logging.Logger) (board.Board, error) {
 	return piImpl.NewPigpio(ctx, conf.ResourceName(), conf, logger)
-}
-
-func SigStr(sig syscall.Signal) string {
-
-	switch sig {
-	case syscall.SIGHUP:
-		return "SIGHUP"
-	case syscall.SIGINT:
-		return "SIGINT"
-	case syscall.SIGQUIT:
-		return "SIGQUIT"
-	case syscall.SIGABRT:
-		return "SIGABRT"
-	case syscall.SIGUSR1:
-		return "SIGUSR1"
-	case syscall.SIGUSR2:
-		return "SIGUSR2"
-	case syscall.SIGTERM:
-		return "SIGTERM"
-	default:
-		return "<UNKNOWN>"
-	}
 }
