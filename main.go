3caf6bccd3247353f5017910d5e76fc805f2885a
Susmita Sanyal <susmita@viam.com>
2024-06-28 15:24:33 -0400

    removing debug code

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"os/signal"
 	"pi-module/pi"
-	"syscall"
 
 	"go.viam.com/rdk/components/board"
 	"go.viam.com/rdk/logging"
@@ -19,11 +15,6 @@ func main() {
 }
 
 func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) error {
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGABRT, syscall.SIGQUIT, syscall.SIGKILL)
-
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
 
 	pigpio, err := module.NewModuleFromArgs(ctx, logger)
 	if err != nil {
@@ -31,28 +22,7 @@ func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) err
 	}
 	pigpio.AddModelFromRegistry(ctx, board.API, pi.Model)
 
-	err = pigpio.Start(ctx)
-	if err != nil {
-		return err
-	}
-	fmt.Print("HEEEEEEEELP!!!")
-	go func() {
-		select {
-		case sig := <-signalChan:
-			logger.Infof("Received signal: %s", sig)
-
-			file, err := os.Create("received_signal.txt")
-			if err != nil {
-				logger.Errorf("Failed to create file: %v", err)
-			} else {
-				defer file.Close()
-				_, err = file.WriteString(sig.String())
-				if err != nil {
-					logger.Errorf("Failed to write to file: %v", err)
-				}
-			}
-		}
-	}()
+	pigpio.Start(ctx)
 	defer pigpio.Close(ctx)
 
 	<-ctx.Done()
